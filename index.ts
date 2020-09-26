2c3f5505f2bd8078a32840de749b4cd1bc8a3bf8
fourside <fourside@gmail.com>
2020-09-26 11:23:16 +0900

    validate fixture

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ type Fixture = {
 
 (async () => {
   const { fixture } = await import(INPUT_FILE);
+  validateFixture(fixture);
   const insertSqlString = insertSql(fixture as Fixture);
   console.log(insertSqlString);
   outputSql(insertSqlString, OUTPUT_INSERT_SQL_FILE);
@@ -68,4 +69,41 @@ function convert(value: SqlValue): string | number {
 
 function outputSql(sql: string, filePath: string): void {
   fs.writeFileSync(filePath, sql, "utf-8");
+}
+
+function validateFixture(fixture: Fixture): true {
+  const tableNames = Object.keys(fixture);
+  if (!tableNames || tableNames.length === 0) {
+    throw new Error("no table names");
+  }
+  tableNames.forEach((tableName) => {
+    const records = fixture[tableName];
+    if (!Array.isArray(records)) {
+      throw new Error(`set Array in ${tableName}`);
+    }
+    if (records.length === 0) {
+      throw new Error(`no records in ${tableName}`);
+    }
+    const columns = Object.keys(records[0]).sort();
+    records.forEach((record, index) => {
+      if (typeof record !== "object") {
+        throw new Error(`record at ${index} in ${tableName} is not in object shape`);
+      }
+      const recordColumns = Object.keys(record);
+      if (recordColumns.length === 0) {
+        throw new Error(`record at ${index} in ${tableName} is empty`);
+      }
+      if (columns.length !== recordColumns.length) {
+        throw new Error(`columns of record at ${index} in ${tableName} is different from the first one`);
+      }
+      recordColumns.sort().forEach((column, columnIndex) => {
+        if (columns[columnIndex] !== column) {
+          throw new Error(
+            `a column of record at ${index} in ${tableName} is different: expected is ${column}, actual is ${columns[columnIndex]}`
+          );
+        }
+      });
+    });
+  });
+  return true;
 }
