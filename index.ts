05b969f37babf0d06c1a80e9555bd6a654b4fb96
fourside <fourside@gmail.com>
2020-09-26 10:18:04 +0900

    output sql files / tweak sql format

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import * as fs from "fs";
+
 const INPUT_FILE = "./input.tables.ts";
+const OUTPUT_INSERT_SQL_FILE = "./insert.sql";
+const OUTPUT_DELETE_SQL_FILE = "./delete.sql";
 
 type SqlValue = string | number | null;
 type Fixture = {
@@ -9,8 +13,11 @@ type Fixture = {
   const { fixture } = await import(INPUT_FILE);
   const insertSqlString = insertSql(fixture as Fixture);
   console.log(insertSqlString);
+  outputSql(insertSqlString, OUTPUT_INSERT_SQL_FILE);
+
   const deleteSqlString = deleteSql(fixture as Fixture);
   console.log(deleteSqlString);
+  outputSql(deleteSqlString, OUTPUT_DELETE_SQL_FILE);
 })();
 
 function insertSql(fixture: Fixture): string {
@@ -23,12 +30,13 @@ function insertSql(fixture: Fixture): string {
       });
       return `( ${values.join(", ")} )`;
     });
-    const sql = `
-INSERT INTO ${tableName} ( ${columns.join(", ")} )
-  VALUES ${valuesList.join(", ")};`;
+    const sql = `INSERT INTO ${tableName} ( ${columns.join(", ")} )
+  VALUES
+    ${valuesList.join(",\n    ")}
+;`;
     return sql;
   });
-  return sqls.join();
+  return sqls.join("\n");
 }
 
 function deleteSql(fixture: Fixture): string {
@@ -56,4 +64,8 @@ function convert(value: SqlValue): string | number {
     return "null";
   }
   return value;
+}
+
+function outputSql(sql: string, filePath: string): void {
+  fs.writeFileSync(filePath, sql, "utf-8");
 }
